7d339d7aa64d96528594ed203ba32bfbf1686eee
misakal092 <misakal1992@gmail.com>
2018-06-04 22:34:20 +0200

    prueba

--- prueba1/src/prueba1/Prueba1.java
+++ prueba1/src/prueba1/Prueba1.java
@@ -16,6 +16,7 @@ public class Prueba1 {
      */
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Que tal estas?");
     }
     
 }
